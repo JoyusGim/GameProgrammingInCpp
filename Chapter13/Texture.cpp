ba5b0c3005da47ef5c633b17798cdfe426aadf06
JoyusGim <Joyus.gim@gmail.com>
2021-07-12 08:17:40 +0900

    텍스처 밉맵

--- Chapter13/Texture.cpp
+++ Chapter13/Texture.cpp
@@ -53,8 +53,17 @@ bool Texture::Load(const std::string& fileName)
 
     SOIL_free_image_data(image);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    if (GLEW_EXT_texture_filter_anisotropic)
+    {
+        GLfloat largest;
+        glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &largest);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, largest);
+    }
 
     return true;
 }
